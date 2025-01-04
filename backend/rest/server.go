1e2d7b3c86c51bc7c9bdc6c79666cfb7d744b6a3
Ikuya Nagaki <ikuya313198@gmail.com>
2025-01-04 16:21:37 +0900

    fix

--- backend/rest/server.go
+++ backend/rest/server.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/eteRnaL198/prasaku/rest/handler"
 )
 
 func StartServer() {
-	handler := NewHandler()
+	handler := handler.NewHandler()
 	fmt.Println("Server is running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", handler))
 }
